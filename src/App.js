3281c38d569aae6a3cc0d46a7cfb3f395ebacb15
loicbdev <loicbodolec@msn.com>
2020-12-16 17:00:31 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
+import SignIn from './components/SignIn.js';
+import Navigation from './components/Navigation.js';
+import 'fontsource-roboto';
+
+const DIV = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+`;
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <DIV className="App">
+  <SignIn />
+  </DIV>
   );
 }
 
