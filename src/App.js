88a63b3f1901edcf270ba0ef2eda665e05dc482c
loicbdev <loicbodolec@msn.com>
2020-12-17 12:36:32 +0100

    fin tuto et MAJ code

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
-import SignIn from './components/SignIn.js';
-import 'fontsource-roboto';
+// source : https://www.youtube.com/watch?v=D3tB_DGgICE&feature=youtu.be
 
-const DIV = styled.div`
+import React, { Component } from 'react';
+import styled from 'styled-components';
+// ou import Button from '@material-ui/core/Button';
+import { Button } from '@material-ui/core';
+import { MuiThemeProvider, createMuiTheme, withStyles } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+
+
+const DIV = styled.div `
 display: flex;
 flex-direction: column;
 align-items: center;
-
+padding: 5rem;
 `;
 
-function App() {
-  
-  return (
-  <DIV className="App">
-  <SignIn />
-  </DIV>
-  );
+const styles = {
+  myLeftButton: {
+    backgroundColor: "blue",
+  }
 }
 
-export default App;
+const theme = createMuiTheme({
+  palette: {
+    primary: blue
+  },
+  typography: {
+    fontSize: 20,
+    fontFamily : "Roboto, sans serif"
+  },
+  overrides: {
+    MuiButton: {
+      root:{
+      backgroundColor: "red",
+      "&:hover": { backgroundColor: "yellow" }
+      }
+    }
+  }
+  })
+
+class App extends Component {
+  render() {
+    return ( 
+      <MuiThemeProvider theme={ theme }>
+        <DIV className = "App">
+          <Button className={this.props.classes.myLeftButton}>Coucou</Button>
+          <Button>les gens !</Button>
+        </DIV>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default withStyles(styles)(App);
